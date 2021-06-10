50128cdf5acb8e7f1fd6307d04d67f677652f7e3
Baurain Olivier <baurain.o@gmail.com>
2021-06-10 14:09:47 +0200

    fix(router): switch to hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -11,19 +11,19 @@ const routes = [
     name: 'Home',
     component: Home,
   },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
   { path: '*', name: 'Error404', component: Error404 },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes,
 });
