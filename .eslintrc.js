de1c421a7c0d4fdb2036ecbfa246de530ec5aed2
Baurain Olivier <baurain.o@gmail.com>
2021-06-10 13:29:49 +0200

    chore(eslint): update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
   ],
   parserOptions: {
@@ -13,5 +13,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-return-assign': 'off'
   },
 };
