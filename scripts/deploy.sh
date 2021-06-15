b836ef227608553d9389dafd9d63b88c87291b27
Baurain Olivier <baurain.o@gmail.com>
2021-06-15 14:53:06 +0200

    Deploy to gh-pages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,4 +12,8 @@ cd dist
 # if you are deploying to a custom domain
 awk -F'"' '/"domain": ".+"/{ print $4; exit; }' ../package.json > CNAME
 
+git add --all
+git commit -m "Deploy to gh-pages"
+git push origin gh-pages
+
 cd -
