953be26a25e48ecb8520b60027dc91d677c1a09c
Baurain Olivier <baurain.o@gmail.com>
2021-06-15 15:23:53 +0200

    fix: remove NFTCreator useless module

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,47 @@
 <template>
-  <n-f-t-creator />
+  <v-container
+    fluid
+    style="max-width:550px;"
+    class="mb-12"
+  >
+    <v-img
+      src="/logos/nft_creator/nft_creator_white.svg"
+      height="55"
+      contain
+      class="mb-3"
+    />
+    <p class="text-center">
+      An easy and quick way to create NFTs on Solana blockchain.<br>
+      See
+      <router-link
+        to="/about"
+        class="text-decoration-none"
+      >
+        how it works
+      </router-link>
+    </p>
+    <v-divider
+      class="my-6"
+    />
+
+    <app-wallet-btn />
+
+    <v-divider
+      class="my-6"
+    />
+
+    <n-f-t-creator-form />
+  </v-container>
 </template>
 
 <script>
-import NFTCreator from '../components/NFTCreator.vue';
+import NFTCreatorForm from '@/components/NFTCreatorForm.vue';
+import AppWalletBtn from '@/components/AppWalletBtn.vue';
 
 export default {
   name: 'Home',
-
   components: {
-    NFTCreator,
+    AppWalletBtn, NFTCreatorForm,
   },
 };
 </script>
