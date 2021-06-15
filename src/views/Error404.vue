d7aa858f205ca1f59c4de05995a9936b806d85c8
Baurain Olivier <baurain.o@gmail.com>
2021-06-15 14:33:30 +0200

    feat(404): add go to home link

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-    Error 404.
-  </div>
+  <v-container
+    fluid
+    style="max-width:550px;"
+    class="mb-12"
+  >
+    <div>
+      Error 404.
+    </div>
+    <router-link
+      class="text-decoration-none"
+      to="/"
+    >
+      Go to home
+    </router-link>
+  </v-container>
 </template>
 
 <script>
