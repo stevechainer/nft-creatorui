4205a3dcdfc409d743847cc8bd77cc125830034a
Baurain Olivier <baurain.o@gmail.com>
2021-07-15 20:03:30 +0200

    fix: fix confirmation commitment

--- src/utils/createTokenCollectible.js
+++ src/utils/createTokenCollectible.js
@@ -84,8 +84,8 @@ export default async function createTokenCollectible(connection, wallet, supply)
     const signature = await connection.sendRawTransaction(signed.serialize());
 
     // Awaiting confirmation
-    await new Promise((r) => setTimeout(r, 60000)); // Add sleep to avoid confirmTransaction timeout
-    await connection.confirmTransaction(signature);
+    await new Promise((r) => setTimeout(r, 20000)); // Add sleep to avoid confirmTransaction timeout
+    await connection.confirmTransaction(signature, 'processed');
 
     return {
       address: mintAccount.publicKey,
