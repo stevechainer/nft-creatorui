92d93979a4d7b4ad1ee069ce7de48f7084194de9
Baurain Olivier <baurain.o@gmail.com>
2021-08-05 23:25:58 +0200

    feat(components): update Q&A component

--- src/components/AboutQA.vue
+++ src/components/AboutQA.vue
@@ -7,8 +7,8 @@
             What is an NFT?
           </div>
           <div>
-            A non-fungible token (NFT) is a unit of data stored on a digital ledger,
-            called a blockchain, that certifies a digital asset to be unique and therefore not interchangeable.
+            A non-fungible token (NFT) is a unit of data stored on a blockchain,
+            that certifies a digital asset to be unique and therefore not interchangeable.
             You can find more information <a
               class="text-decoration-none"
               href="https://en.wikipedia.org/wiki/Non-fungible_token"
@@ -20,33 +20,7 @@
         <br>
         <li>
           <div class="font-weight-bold">
-            How do I sell my NFT?
-          </div>
-          <div>
-            You have to create a Serum market with the NFT mint. More details <a
-              class="text-decoration-none"
-              href="https://serum-academy.com/en/add-market/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >here</a>.<br>
-            Be aware that creating a Serum market can cost up to 15 SOL.<br>
-            We plan to add an option to create a Serum market directly from NFTCreator.
-          </div>
-        </li>
-        <br>
-        <li>
-          <div class="font-weight-bold">
-            Can I mint new tokens after creating my NFT?
-          </div>
-          <div>
-            No, because when the NFT was created, your mint right was revoked.
-            We plan to add a feature that will allow not to revoke the mint right at creation, so the supply will not be fixed.
-          </div>
-        </li>
-        <br>
-        <li>
-          <div class="font-weight-bold">
-            Why are there network fees?
+            I heard that Solana transactions were almost free, why are there network fees?
           </div>
           <div>
             To create an NFT you must first create a token and then create an account that could receive this token.
@@ -63,11 +37,11 @@
           <div>
             All images are stored on the <a
               class="text-decoration-none"
-              href="https://ipfs.io/"
+              href="https://www.arweave.org/"
               target="_blank"
               rel="noopener noreferrer"
-            >IPFS protocol</a>.
-            It provides deduplication, high performance, and clustered persistence
+            >Arweave protocol</a>.
+            It allows you to store data permanently, sustainably, with a single upfront fee.
           </div>
         </li>
         <br>
@@ -88,62 +62,11 @@
         <br>
         <li>
           <div class="font-weight-bold">
-            Can I change the name or image of my NFT?
-          </div>
-          <div>
-            No, these are definitive fields. You must create a new NFT in this case.
-          </div>
-        </li>
-        <br>
-        <li>
-          <div class="font-weight-bold">
             I just created my NFT, how can I send it to someone?
           </div>
           <div>
-            You can use any traditional wallet (Sollet, Phantom, Ledger, Solong...)
-            Your NFT should be listed with the mint address that was given to you when the token was created.
-          </div>
-        </li>
-        <br>
-        <li>
-          <div class="font-weight-bold">
-            The supply of some NFTs is higher than 1, is this normal?
-          </div>
-          <div>
-            If the supply of an NFT is greater than 1,
-            it is not really an NFT (non-fungible token) but a "semi-fungible token".
-          </div>
-        </li>
-        <br>
-        <li>
-          <div class="font-weight-bold">
-            Where can I find the list of all NFTs created on NFTCreator?
-          </div>
-          <div>
-            The list of all NFTs created on NFTCreator is available at: <a
-              class="text-decoration-none"
-              href="https://api.nft-creator.sonar.watch/collectibles"
-              target="_blank"
-              rel="noopener noreferrer"
-            >https://api.nft-creator.sonar.watch/collectibles</a>
-          </div>
-        </li>
-        <br>
-        <li>
-          <div class="font-weight-bold">
-            Why does my NFT link start with <code>ipfs://</code>?
-          </div>
-          <div>
-            IPFS data are permanent.
-            If you put a HTTP link to your asset, you run the risk of that URL not being available at some point in the future.
-            We reference IPFS assets with the <code>ipfs://YOUR_CID pointer</code>.
-            So it's easy to pull out the CID and load the content using a public gateway.<br>
-            You can find a list of public gateways <a
-              class="text-decoration-none"
-              href="https://ipfs.github.io/public-gateway-checker/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >here</a>.
+            Your NFT should be listed on your as a traditional token.
+            In some wallets it will be listed directly as an NFT (e.g. Phantom)
           </div>
         </li>
       </ul>
